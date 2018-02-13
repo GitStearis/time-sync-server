d98db6f16d72ff7a1b0090ab963155cb6cafa192
George Puisha <stearis@tut.by>
2018-02-13 10:55:20 +0300

    Change structure

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-var express = require('express');
 var timesyncServer = require('timesync/server');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-var port = process.env.PORT || 3000;
+var PORT = 8081;
+
 var app = express();
-app.listen(port);
-console.log('Server listening at http://localhost:' + port);
-
-app.get('/', express.static(__dirname));
-
+app.use(bodyParser.json()); // for parsing application/json
 app.use('/timesync', timesyncServer.requestHandler);
+
+app.post('/timesync', function(req, res) {
+    var data = {
+        id: (req.body && 'id' in req.body) ? req.body.id : null,
+        result: Date.now()
+    };
+    res.json(data);
+});
+
+app.get('/timesync', function(req, res) {
+    res.json(Date.now());
+});
+
+app.listen(PORT);
+console.log('Server listening at http://localhost:' + PORT);
